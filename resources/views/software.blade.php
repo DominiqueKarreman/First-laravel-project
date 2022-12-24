c9bc9fef6e6bab74a830abafa6559c7ad04c6948
Dominique Karreman <s1169919@student.windesheim.nl>
2022-12-24 16:15:53 +0100

    override

---
+++ resources/views/software.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div id="skillsDiv" class="skillsDiv">
+        <div class="skills">
+          <div class="skill">
+            <img class="skillIMG" src={htmlcssjs} alt="img" />
+            <h1 class="year">2021</h1>
+            <h1 class="month">September</h1>
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+            <h1 id="titleFix2" class="skillTitle">
+              HTML Css Javascript
+            </h1>
+          </div>
+  
+          <div class="skill">
+            <img class="skillIMG" src={node} alt="img" />
+            <h1 class="year">2021</h1>
+            <h1 class="month">November</h1>
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+            <h1 id="titleFix2" class="skillTitle">
+              Node + ExpressJS
+            </h1>
+          </div>
+          <div class="skill">
+            <img class="skillIMG" src={react2} alt="img" />
+            <h1 class="year">2022</h1>
+            <h1 class="month">January</h1>
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+            <h1 class="skillTitle">React</h1>
+          </div>
+          <div class="skill">
+            <img src={python} class="skillIMG" alt="img" />
+            <h1 class="month">April</h1>
+            <h1 class="year">2022</h1>
+            <div id="lineFix" class="line2"></div>
+            <div id="lineFix2" class="line3"></div>
+            <h1 id="titleFix" class="skillTitle">
+              Python
+            </h1>
+          </div>
+        </div>
+        <div class="infoSection">
+          <div class="infodiv">
+  
+          <h1 class="bigInfoTitle">Skills</h1>
+          <h1 class="infoTitle">My aquired programming skillset!</h1>
+          <p class="infoText">
+            I have been learning programming for about 1.5 years now. I have
+            experience with HTML, CSS, Javascript, NodeJS, ExpressJS, React and
+            Python. I am currently learning React and Python. I am a Fullstack developer
+            and i also dable a bit in UI/UX design. I am currently looking for a
+            job as a junior developer.
+            
+          </p>
+          </div>
+        </div>
+      </div>
+</body>
+</html>
